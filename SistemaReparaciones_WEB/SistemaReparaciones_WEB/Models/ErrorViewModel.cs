bb9435a76f78bba1854fea4961ae337a2f9ba5b0
BryanSaJi <bsanabriaj@hotmail.com>
2024-02-19 16:55:53 -0600

    R00 Project Created

---
+++ SistemaReparaciones_WEB/SistemaReparaciones_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaReparaciones_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
